fc560c753dd3627c997aa57d8127a788768a32e6
houdaaoussar <97853285+houdaaoussar@users.noreply.github.com>
2022-01-17 21:51:41 +0100

    Add files via upload

---
+++ cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cave" tilewidth="16" tileheight="16" tilecount="49" columns="7">
+ <image source="../../Downloads/objects.png" trans="000000" width="112" height="112"/>
+</tileset>
